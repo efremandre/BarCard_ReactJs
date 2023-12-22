46d6c70c0556858a4390b8150a4d7d1788fc31d5
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-12-22 12:37:32 +0300

    add deploy

--- src/components/Main/Cards/index.jsx
+++ src/components/Main/Cards/index.jsx
@@ -1,10 +1,11 @@
 import style from './Cards.module.scss'
 
 const Cards = (props) => {
-
+	// eslint-disable-next-line react/prop-types
 	const preloader = props.loading ? <div className={style.loading}>Loading...</div>
 		: null
 
+	// eslint-disable-next-line react/prop-types
 	const li = props.cards.map(el => (
 		<li className={style.card} key={el.idDrink}>
 			<div className={style.card__item}>
@@ -17,7 +18,10 @@ const Cards = (props) => {
 						<span className={style.card__id}>art. {el.idDrink}</span>
 					</div>
 					<div className={style.card__buttons}>
-						<button className={style.card__like} onClick={() => props.like(el.idDrink)}>
+						<button className={style.card__like} onClick={
+							// eslint-disable-next-line react/prop-types
+							() => props.like(el.idDrink)
+						}>
 							<svg className={style.card__svg} viewBox="0 0 24 22" fill="none" xmlns="http://www.w3.org/2000/svg">
 								<path className={style.card__svgPath} d="M12 8.22892C12.234 7.10892 13.547 1.99992 17.382 1.99992C19.602 1.99992 22 3.55092 22 7.00292C22 10.9099 18.373 15.4729 12 19.6319C5.627 15.4729 2 10.9099 2 7.00292C2 3.51892 4.369 1.99792 6.577 1.99792C10.5 1.99792 11.722 7.12392 12 8.22892ZM0 7.00292C0 11.0709 3.06 16.4839 12 21.9999C20.94 16.4839 24 11.0709 24 7.00292C24 -0.959077 14.352 -2.02508 12 3.26592C9.662 -1.99608 0 -1.00408 0 7.00292Z" fill={el.isLike ? 'red' : '#fff'} />
 								<path d="M12 8.22892C12.234 7.10892 13.547 1.99992 17.382 1.99992C19.602 1.99992 22 3.55092 22 7.00292C22 10.9099 18.373 15.4729 12 19.6319C5.627 15.4729 2 10.9099 2 7.00292C2 3.51892 4.369 1.99792 6.577 1.99792C10.5 1.99792 11.722 7.12392 12 8.22892Z" fill={el.isLike ? 'red' : ''} />
@@ -34,7 +38,10 @@ const Cards = (props) => {
 			{preloader}
 			<div className="container">
 				<ul className={style.cards}>
-					{props.isLikedArrayCardsEmpty ? <div className={style.emptyCards}>Favorites are empty</div> : li}
+					{
+						// eslint-disable-next-line react/prop-types
+						props.isLikedArrayCardsEmpty ? <div className={style.emptyCards}>Favorites are empty</div> : li
+					}
 				</ul>
 			</div>
 		</section>
